652d7aac643ee0377b19a691461508bbdc2c4881
DESKTOP-9LAVD5\jarvis <eran.rav@gmail.com>
2017-08-03 11:50:01 +0300

    app beta - save hostname and port

--- volume_remote/Assets/RemoteController.cs
+++ volume_remote/Assets/RemoteController.cs
@@ -2,9 +2,12 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RemoteController : MonoBehaviour {
+public class RemoteController : MonoBehaviour
+{
 
-    public string url = "http://10.0.0.1:8006/?";
+    private string hostname;
+    private string port;
+    private string url;
     private string urlMethodAdd = "method=add&";
     private string urlMethodSet = "method=set&";
     private string urlAmount = "vol=";
@@ -18,11 +21,21 @@ public class RemoteController : MonoBehaviour {
     public Button startButton;
     public Button stopButton;
     public Slider slider;
-    public Text debugText;
+    //public Text debugText;
+    public Text hostnameText;
+    public Text portText;
 
     void Start()
     {
 
+        hostname = PlayerPrefs.GetString("hostname", "http://10.0.0.1");
+        port = PlayerPrefs.GetString("port", "8006");
+        url = hostname + ":" + port + "/?";
+        hostnameText.GetComponent<InputField>().text = hostname;
+        portText.GetComponent<InputField>().text = port;
+
+        hostnameText.GetComponent<InputField>().onValueChanged.AddListener(onHostnameChanged);
+        portText.GetComponent<InputField>().onValueChanged.AddListener(onPortChanged);
         plusButton.onClick.AddListener(onPlusButtonClicked);
         minusButton.onClick.AddListener(onMinusButtonClicked);
         startButton.onClick.AddListener(onStartButtonClicked);
@@ -32,17 +45,31 @@ public class RemoteController : MonoBehaviour {
 
     void Update()
     {        
-        debugText.text = Input.acceleration.x.ToString();
+        //debugText.text = /*"x: " + Input.acceleration.x.ToString() + */" y: " + Input.acceleration.y.ToString()/* + " z: " + Input.acceleration.z.ToString()*/;
 
-        if (doLiveUpdate)
+        if (doLiveUpdate && Mathf.Abs(Input.acceleration.y * -1 - slider.value) > 0.01f)
         {
-            slider.value = Input.acceleration.y;
-            WWW www = new WWW(url + urlMethodSet + urlAmount + (slider.value*100));
+            slider.value = Input.acceleration.y * -1;
+            WWW www = new WWW(url + urlMethodSet + urlAmount + (int)(slider.value*100));
         }
         else
         {
             slider.value = 0;
         }
+    }
+
+    void onHostnameChanged(string text)
+    {
+        hostname = text;
+        url = hostname + ":" + port + "/?";
+        PlayerPrefs.SetString("hostname", hostname);
+    }
+
+    void onPortChanged(string text)
+    {
+        port = text;
+        url = hostname + ":" + port + "/?";
+        PlayerPrefs.SetString("port", port);
     }
 
     void onStartButtonClicked()
